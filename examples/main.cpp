cd3d9c7653ae67f62458ed0fa558c4bac2b5cd18
Anastasia <vernidub.anastasya@yandex.ru>
2016-10-31 21:27:20 +0300

    Update main.cpp

--- examples/main.cpp
+++ examples/main.cpp
@@ -1,1 +1,16 @@
+#include "VkClient.h"
+#include <iostream>
+#include <map>
+#include <string>
+//client_id=5685143
+int main() {
 
+	setlocale(LC_ALL, "rus");
+	std::string access_token;
+	std::cout << "Hello! If you want to get the access_token please check the link below" << std::endl;
+	std::cout << "https://oauth.vk.com/authorize?client_id=5685143&display=page&redirect_uri=http://vk.com/callback&scope=friends&response_type=token&v=5.59&state=1" << std::endl;
+	std::cout << "Access_token = ";
+	std::cin >> access_token;
+	Vk::VkClient data({{"token", access_token }});
+	data.check_connection();
+}
