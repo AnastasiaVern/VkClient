80c804b89f9dab758f730403ee58f1e6aafc7115
Anastasia <vernidub.anastasya@yandex.ru>
2016-10-31 22:20:24 +0300

    Update client.hpp

--- include/vk/client.hpp
+++ include/vk/client.hpp
@@ -1,33 +1,15 @@
 #include <map>
 #include <string>
-#include <iostream>
-#include <curl/curl.h>
-using dict_t = std::map<std::string, std::string>;
 namespace Vk {
 	class VkClient {
+	public:
+		using dict_t = std::map<std::string, std::string>;
+
+		VkClient() {};
+		VkClient(dict_t settings) : settings_(settings) {};
+		auto check_connection() -> bool;
+
 	private:
 		dict_t settings_;
-	public:
-		VkClient() {};
-		VkClient(dict_t settings) 
-		{
-			settings_ = settings;
-		}
-
-		auto check_connection() -> bool 
-		{
-			CURL *curl = curl_easy_init();
-			if (curl) {
-				CURLcode res;
-				curl_easy_setopt(curl, CURLOPT_URL, "https://api.vk.com/method/account.getInfo()");
-
-				res = curl_easy_perform(curl);
-				if (res == CURLE_OK) {
-					std::cout << "Connected successfully"<< std::endl;
-					return true;
-				}
-				curl_easy_cleanup(curl);
-			}
-		};
 	};
 }
