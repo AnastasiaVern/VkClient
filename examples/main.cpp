241989657ae00ea6eec03901f9705c937d84fcae
Anastasia <vernidub.anastasya@yandex.ru>
2016-10-31 21:29:34 +0300

    Update main.cpp

--- examples/main.cpp
+++ examples/main.cpp
@@ -9,7 +9,7 @@ int main() {
 	std::string access_token;
 	std::cout << "Hello! If you want to get the access_token please check the link below" << std::endl;
 	std::cout << "https://oauth.vk.com/authorize?client_id=5685143&display=page&redirect_uri=http://vk.com/callback&scope=friends&response_type=token&v=5.59&state=1" << std::endl;
-	std::cout << "Access_token = ";
+	std::cout << "Access_token: ";
 	std::cin >> access_token;
 	Vk::VkClient data({{"token", access_token }});
 	data.check_connection();
