66138e8189513a7e669892015ba40f7ed9a19190
Anastasia <vernidub.anastasya@yandex.ru>
2016-11-10 23:13:24 +0300

    Update client.hpp

--- include/vk/client.hpp
+++ include/vk/client.hpp
@@ -1,5 +1,7 @@
 #include <map>
 #include <string>
+#include <iostream>
+#include <curl/curl.h>
 namespace Vk {
 	class VkClient {
 	public:
@@ -8,8 +10,11 @@ namespace Vk {
 		VkClient() {};
 		VkClient(dict_t settings) : settings_(settings) {};
 		auto check_connection() -> bool;
-
+		auto groups_get()-> void;
+		auto friends_getAppUsers()->void;
+		static auto func(char* ptr, size_t size, size_t nmemb, std::string* link)->size_t;
 	private:
 		dict_t settings_;
 	};
 }
+
