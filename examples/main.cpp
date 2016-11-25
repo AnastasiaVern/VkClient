d9b57c0b30b9b4e0acf87fd68148d535174a7aa0
Anastasia <vernidub.anastasya@yandex.ru>
2016-11-25 13:43:34 +0300

    Update main.cpp

--- examples/main.cpp
+++ examples/main.cpp
@@ -2,19 +2,32 @@
 #include <iostream>
 #include <map>
 #include <string>
-//client_id=5685143
+#include <thread>
+
 int main() {
 
 	setlocale(LC_ALL, "rus");
+	std::string flag = "-v";
+	std::string users_flag;
 	std::string access_token;
 	std::cout << "Hello! If you want to get the access_token please check the link below" << std::endl;
 	std::cout << "https://oauth.vk.com/authorize?client_id=5685143&display=page&redirect_uri=http://vk.com/callback&scope=friends&response_type=token&v=5.59&state=1" << std::endl;
 	std::cout << "Access_token: ";
 	std::cin >> access_token;
-	Vk::VkClient data({{"token", access_token }});
-	data.check_connection();
-	//data.get_groups();
-	data.print_groups(data.get_groups());
-        data.start_streaming(4);
-	//token for 24.11.2016 219eaf4157e83b3e234c95bdcc2ca09804d4d6b2a8a9b3cd6a8d90e625322ed35f022dcfe9efa838eaeab
+	Vk::VkClient data({ { "token", access_token } });
+	if (data.check_connection() == TRUE) 
+	{
+		int n;
+		data.print_groups(data.get_groups());
+		std::cout << "Please enter the number of threads: " << std::endl;
+		std::cin >> n;
+		std::cout << "Flag: ";
+		std::cin >> users_flag;
+		if (flag == users_flag)
+		{
+			data.start_streaming(n);
+		}
+	};
+	system("pause");
+	return 0;
 }
